9ccb0ac5c401fe3f10a6299e9e7bc2adb16e98f9
Hongliang TIAN <tatetian@gmail.com>
2013-05-11 23:11:28 +0800

    Fixed bug in CalculateMean

--- src/main/java/me/tatetian/hs/jobs/meancalculation/CalculateMeanWithIndexedSampling.java
+++ src/main/java/me/tatetian/hs/jobs/meancalculation/CalculateMeanWithIndexedSampling.java
@@ -3,6 +3,7 @@ package me.tatetian.hs.jobs.meancalculation;
 import me.tatetian.hs.io.IndexedTextInputFormat;
 
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.util.ToolRunner;
 
 public class CalculateMeanWithIndexedSampling extends CalculateMean {
 	@Override
@@ -11,5 +12,10 @@ public class CalculateMeanWithIndexedSampling extends CalculateMean {
 		System.out.println(msg);
 		System.err.println(msg);
 		job.setInputFormatClass(IndexedTextInputFormat.class);
+	}
+	
+	public static void main(String[] args) throws Exception {
+		int res = ToolRunner.run(new CalculateMeanWithIndexedSampling(), args);
+		System.exit(res);
 	}
 }
--- src/main/java/me/tatetian/hs/jobs/meancalculation/CalculateMeanWithNaiveSampling.java
+++ src/main/java/me/tatetian/hs/jobs/meancalculation/CalculateMeanWithNaiveSampling.java
@@ -1,6 +1,7 @@
 package me.tatetian.hs.jobs.meancalculation;
 
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.util.ToolRunner;
 
 public class CalculateMeanWithNaiveSampling extends CalculateMean {
 	@Override
@@ -10,5 +11,10 @@ public class CalculateMeanWithNaiveSampling extends CalculateMean {
 		System.err.println(msg);
 		job.setJarByClass(CalculateMeanWithNaiveSampling.class);
 		job.setMapperClass(CalculateMeanMapperWithNaiveSampling.class);
+	}
+				
+	public static void main(String[] args) throws Exception {
+		int res = ToolRunner.run(new CalculateMeanWithNaiveSampling(), args);
+		System.exit(res);
 	}
 }
